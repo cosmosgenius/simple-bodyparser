95b65c1bded2b7e564777b868b589950753ea0b2
Sharat M R <cosmosgenius@gmail.com>
2014-08-23 19:42:50 +0530

    added options

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 /*jslint node: true */
 'use strict';
 
-module.exports = function(req, res, next) {
-    var data = '';
-    req.setEncoding('utf8');
-    req.on('data', function(chunk) {
-        data += chunk;
-    });
-    req.on('end', function() {
-        req.rawBody = data;
-        next();
-    });
+module.exports = function(options) {
+    return function(req, res, next) {
+        var data = '';
+        req.setEncoding('utf8');
+        req.on('data', function(chunk) {
+            data += chunk;
+        });
+        req.on('end', function() {
+            req.rawBody = data;
+            next();
+        });
+    };
 };
+
+
