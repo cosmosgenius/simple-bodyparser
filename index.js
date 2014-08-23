ae9ab76d3078bcd401cc25fc8a6c3243168d3c69
Sharat M R <cosmosgenius@gmail.com>
2014-08-23 19:32:39 +0530

    added rawbody code

---
+++ index.js
@@ -0,0 +1,14 @@
+/*jslint node: true */
+'use strict';
+
+module.exports = function(req, res, next) {
+    var data = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', function() {
+        req.rawBody = data;
+        next();
+    });
+};
