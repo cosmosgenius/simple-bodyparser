96d6c3d48e147408e95e41500c84386c5562910c
Sharat M R <cosmosgenius@gmail.com>
2014-08-23 22:57:03 +0530

    added unit test case

--- test/index.js
+++ test/index.js
@@ -4,14 +4,37 @@
 'use strict';
 
 var rawbody = require('../'),
+    request = require('supertest'),
+    app = require('express')(),
     should = require('should');
 
+
 describe('rawbody', function() {
+    before(function(){
+        request = request(app);
+    });
+
     it('should exist', function() {
         should.exist(rawbody);
     });
 
     it('should return a function', function() {
         rawbody().should.be.a.Function;
+    });
+
+    it('should return a data', function(done) {
+        var testDtr = 'hello this is a test';
+        app.use(rawbody());
+
+        app.post('/',function(req) {
+            req.rawBody.should.eql(testDtr);
+            done();
+        });
+
+        request
+            .post('/')
+            .send(testDtr)
+            .expect(200)
+            .end();
     });
 });
